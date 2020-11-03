dbde56e5133fa20a8fdd675ff76dcba21f2623f1
Jeremy Sayers <jeremy1080@gmail.com>
2020-11-03 17:18:02 -0500

    Part 1 initial setup.

---
+++ Part 1 - Visual Studio Setup/Program.cs
@@ -0,0 +1,79 @@
+﻿using SDL2;
+using System;
+
+namespace SDLCSTutorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Initilizes SDL.
+            if (SDL.SDL_Init(SDL.SDL_INIT_VIDEO) < 0)
+            {
+                Console.WriteLine($"There was an issue initilizing SDL. {SDL.SDL_GetError()}");
+            }
+
+            // Create a new window given a title, size, and passes it a flag indicating it should be shown.
+            var window = SDL.SDL_CreateWindow("SDL .NET 5 Tutorial", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+            if (window == IntPtr.Zero)
+            {
+                Console.WriteLine($"There was an issue creating the window. {SDL.SDL_GetError()}");
+            }
+
+            // Creates a new SDL hardware renderer using the default graphics device with VSYNC enabled.
+            var renderer = SDL.SDL_CreateRenderer(window, 
+                                                  -1, 
+                                                  SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED | 
+                                                  SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+            if (renderer == IntPtr.Zero)
+            {
+                Console.WriteLine($"There was an issue creating the renderer. {SDL.SDL_GetError()}");
+            }
+
+            // Initilizes SDL_image for use with png files.
+            if (SDL_image.IMG_Init(SDL_image.IMG_InitFlags.IMG_INIT_PNG) == 0)
+            {
+                Console.WriteLine($"There was an issue initilizing SDL2_Image {SDL_image.IMG_GetError()}");
+            }
+
+            var running = true;
+
+            // Main loop for the program
+            while (running)
+            {
+                // Check to see if there are any events and continue to do so until the queue is empty.
+                while (SDL.SDL_PollEvent(out SDL.SDL_Event e) == 1)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            running = false;
+                            break;
+                    }
+                }
+
+                // Sets the color that the screen will be cleared with.
+                if (SDL.SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255) < 0)
+                {
+                    Console.WriteLine($"There was an issue with setting the render draw color. {SDL.SDL_GetError()}");
+                }
+
+                // Clears the current render surface.
+                if (SDL.SDL_RenderClear(renderer) < 0)
+                {
+                    Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
+                }
+
+                // Switches out the currently presented render surface with the one we just did work on.
+                SDL.SDL_RenderPresent(renderer);
+            }
+
+            // Clean up the resources that were created.
+            SDL.SDL_DestroyRenderer(renderer);
+            SDL.SDL_DestroyWindow(window);
+            SDL.SDL_Quit();
+        }
+    }
+}
